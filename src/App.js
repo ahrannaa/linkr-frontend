40c08523b98f908f6bdc7fd0c375e7b6bb78a63b
Júlia Botelho <cajujumo@gmail.com>
2023-01-04 23:14:26 -0300

    feat: timeline layout

--- src/App.js
+++ src/App.js
@@ -1,1 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import styled from "styled-components";
+import TimelinePage from "./pages/TimelinePage.js" 
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/timeline" element={<TimelinePage />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default App;
+
+
+
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
-import ReactDOM from "react-dom"
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
+
+
+
+
+
+/* import ReactDOM from "react-dom"
 import App from "./App"
     
-ReactDOM.render(<App/>, document.querySelector(".root"))
+ReactDOM.render(<App/>, document.querySelector(".root")) */
