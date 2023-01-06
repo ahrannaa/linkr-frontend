67d2bb4dba945dcb8488fbffe3ef589dadc8e684
Jonas Tiago <jonastiago95@gmail.com>
2023-01-05 22:45:26 -0300

    refact: search component

--- src/components/search.js
+++ src/components/search.js
@@ -3,17 +3,37 @@ import SearchIcon from "../assets/img/Vector.svg";
 
 export default function Search() {
   return (
-    <StyleSearch>
-      <input type="text" placeholder="Search for people" />
-      <button>
-        <img src={SearchIcon} alt="search-icon"/>
-      </button>
-    </StyleSearch>
+    
+      <StyleSearch>
+        <input type="text" placeholder="Search for people" />
+        <button>
+          <img src={SearchIcon} alt="search-icon" />
+        </button>
+        {/* <ResultStyle>
+          <img
+            src="https://st2.depositphotos.com/3765293/6822/i/450/depositphotos_68226509-stock-photo-black-cat-in-the-grass.jpg"
+            alt="avatar user"
+          />
+          <span>João Avatares</span>
+        </ResultStyle>
+        <ResultStyle>
+          <img
+            src="https://st2.depositphotos.com/3765293/6822/i/450/depositphotos_68226509-stock-photo-black-cat-in-the-grass.jpg"
+            alt="avatar user"
+          />
+          <span>João Avatares</span>
+        </ResultStyle> */}
+      </StyleSearch>
+    
   );
 }
 
 const StyleSearch = styled.div`
-    position:relative;
+  background-color: #e7e7e7;
+  border-radius: 8px;
+  position: absolute;
+  left:35%;
+  top:13px;
 
   input {
     width: 563px;
@@ -21,14 +41,28 @@ const StyleSearch = styled.div`
     border-radius: 8px;
     padding: 14px;
     font-size: 19px;
+    border: none;
   }
 
-  button{
-    position:absolute;
-    right:14px;
-    top:13px;
-    background-color:#fff;
-    border:none;
+  button {
+    position: absolute;
+    right: 14px;
+    top: 13px;
+    background-color: #fff;
+    border: none;
     cursor: pointer;
   }
 `;
+
+// const ResultStyle = styled.div`
+//   display: flex;
+//   align-items: center;
+//   margin:16px;
+
+//   img {
+//     width: 39px;
+//     height: 39px;
+//     border-radius: 50%;
+//     margin: 0 10px;
+//   }
+// `;
