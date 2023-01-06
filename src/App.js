798e3f9f67eab8d5f5fac9618a1b2c237c9fdab3
Ahranna Araujo <ahrannaaraujo@ahrannas-air.lan>
2023-01-06 22:39:15 +0000

    delete and modal configuration

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import styled from "styled-components";
-import TimelinePage from "./pages/TimelinePage.js" 
+import TimelinePage from "./pages/TimelinePage.js";
 
 function App() {
     return (
         <BrowserRouter>
-            <Routes>
+           <Routes>
                 <Route path="/timeline" element={<TimelinePage />} />
             </Routes>
         </BrowserRouter>
