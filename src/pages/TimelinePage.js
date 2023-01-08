49ee538bf41e48fb500020391e9d20a03ec62e66
Júlia Botelho <cajujumo@gmail.com>
2023-01-08 16:40:50 -0300

    letsmerge

--- src/pages/TimelinePage.js
+++ src/pages/TimelinePage.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios"
 
 import styled from "styled-components";
 import TopBar from "../components/TopBar.js";
@@ -9,6 +10,26 @@ import Hashtags from "../components/Hashtags.js";
 
 export default function TimelinePage() {
   const [latestPosts, setLatestPosts] = useState([]);
+  const [dataPostReceived, setDataPostReceived] = useState(false)
+
+  useEffect(() => {
+    axios.get("http://localhost:4000/posts")
+      .then(res => {
+        setLatestPosts(res.data)
+        setDataPostReceived(false)
+      })
+      .catch(err => {
+        alert("An error ocurred while trying to fetch the posts, please refresh the page")
+      })
+  }, [dataPostReceived])
+
+  if (!latestPosts){
+    return(
+      <TimelineContent>
+        <h1>Loading...</h1>
+      </TimelineContent>
+    )
+  }
 
   return (
     <>
@@ -17,9 +38,14 @@ export default function TimelinePage() {
         <TopBar />
         <TimelineContent>
           <h1>timeline</h1>
-          <NewPost />
-          <Post />
-          {/* {latestPosts.map((latestPost, index) => <Post key={index} latestPost={latestPost} />)} */}
+          <NewPost dataPostReceived={dataPostReceived} setDataPostReceived={setDataPostReceived} />
+          <>
+          {latestPosts.length === 0? (
+            <h1>There are no posts yet</h1>
+          ):(
+           <> {latestPosts.map((latestPost, index) => <Post key={index} latestPost={latestPost} />)}</>
+          )}
+          </> 
         </TimelineContent>
         <Hashtags />
       </TimelineBackground>
