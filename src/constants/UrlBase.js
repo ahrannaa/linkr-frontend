0b101ec0484bb9511d5742c1a2fee8001a3d8153
Jonas Tiago <jonastiago95@gmail.com>
2023-01-11 10:29:45 -0300

    feat: urlbase

--- src/constants/UrlBase.js
+++ src/constants/UrlBase.js
@@ -1,2 +1,2 @@
-// export const URL_BASE = 'https://linkr-api-0l14.onrender.com'
-export const URL_BASE = 'http://localhost:4000'
+export const URL_BASE = 'https://linkr-api-0l14.onrender.com'
+// export const URL_BASE = 'http://localhost:4000'
